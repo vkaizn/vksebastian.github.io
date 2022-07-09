35158bb543f9c6469dc9410ff99ef99720fa0962
vkaizn <vk.aizn@gmail.com>
2022-07-09 09:54:33 +0800

    Changed formatting on timer and work tab
    Removed the decimals on the displayed time on page and included bullets on the work tab

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -5,8 +5,8 @@
 				const element = document.getElementById("timeInSeconds");
 				const greeting = document.getElementById("greeting");
 				setInterval(function() {
-					element.innerHTML = TimeMe.getTimeOnCurrentPageInSeconds();
-				}, 100);
+					element.innerHTML = TimeMe.getTimeOnCurrentPageInSeconds().toFixed(0);
+				}, 1000);
 				
 				TimeMe.callAfterTimeElapsedInSeconds(10, function(){
 					greeting.innerHTML = "Hire me!";
