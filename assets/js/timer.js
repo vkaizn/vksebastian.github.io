24c038696b523bcf12c32c2bafc2febccc2ecdfb
vkaizn <vk.aizn@gmail.com>
2024-02-21 20:59:42 +0800

    Initial update for 2024

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -5,10 +5,38 @@
 				const element = document.getElementById("timeInSeconds");
 				const greeting = document.getElementById("greeting");
 				setInterval(function() {
-					element.innerHTML = TimeMe.getTimeOnCurrentPageInSeconds().toFixed(0);
+					element.innerHTML = toRomanNumeral(TimeMe.getTimeOnCurrentPageInSeconds().toFixed(0));
 				}, 1000);
 				
 				TimeMe.callAfterTimeElapsedInSeconds(10, function(){
 					greeting.innerHTML = "Hire me!";
-					greeting.setAttribute('href', 'https://vkms.herokuapp.com')
+					greeting.setAttribute('href', 'https://www.linkedin.com/in/vksebastian/')
 				});
+
+				function toRomanNumeral(num) {
+					const romanNumerals = {
+						M: 1000,
+						CM: 900,
+						D: 500,
+						CD: 400,
+						C: 100,
+						XC: 90,
+						L: 50,
+						XL: 40,
+						X: 10,
+						IX: 9,
+						V: 5,
+						IV: 4,
+						I: 1
+					};
+
+					let result = '';
+					for (let key in romanNumerals) {
+						while (num >= romanNumerals[key]) {
+							result += key;
+							num -= romanNumerals[key];
+						}
+					}
+				
+					return result;
+				}
